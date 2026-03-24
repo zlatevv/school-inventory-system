766d3a856c9a110a1dfcc0d883278c9bdcb75d4c
ExamPrep <cvetanzlatev30@gmail.com>
2026-03-24 09:04:58 +0200

    feat. working dockerisation and ability to start project anywhere

--- backend/api-gateway/src/main/java/bg/schoolinventory/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/bg/schoolinventory/apigateway/ApiGatewayApplication.java
@@ -15,22 +15,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-
-        corsConfig.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
-
-        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        corsConfig.setAllowedHeaders(List.of("*"));
-
-        corsConfig.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig); // Важи за всички пътища в Gateway-a
-
-        return new CorsWebFilter(source);
-    }
 }
